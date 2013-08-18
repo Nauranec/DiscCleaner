d1d74e7064e8a5129fa238530889e5ecce96c16e
Nauranec <alexey.platkovsky@gmail.com>
2013-08-19 02:18:45 +0300

    crate "tools" package

--- tools/src/DiscCleaner.java
+++ tools/src/DiscCleaner.java
@@ -1,15 +1,16 @@
 import GUI.MainFrame;
+import tools.FilesList;
 
 public class DiscCleaner {
     public static void main(String[] args) {
         FilesList.fillDir();
         new MainFrame();
-        /*String path[] = FilesList.getDirectories();
+        /*String path[] = tools.FilesList.getDirectories();
 
         for (int i = 0; i < path.length; i++){
             if (path[i] != null){
                 File file = new File(path[i]);
-                Files.delete(file);
+                tools.Files.delete(file);
             }
         }*/
 
--- tools/src/tools/Files.java
+++ tools/src/tools/Files.java
@@ -1,3 +1,5 @@
+package tools;
+
 import java.io.File;
 
 public class Files {
